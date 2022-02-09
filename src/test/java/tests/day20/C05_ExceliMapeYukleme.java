109393e44fb39bd0d2bce5f39bc17d903a2875e8
Abdulbakı Gumus <agummus038@gmail.com>
2022-02-10 02:15:28 +0300

    window_handle

--- src/test/java/tests/day20/C05_ExceliMapeYukleme.java
+++ src/test/java/tests/day20/C05_ExceliMapeYukleme.java
@@ -1,9 +1,11 @@
 package tests.day20;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.ReusableMethods;
 
 import java.io.FileNotFoundException;
+import java.util.Map;
 
 public class C05_ExceliMapeYukleme {
     @Test
@@ -12,7 +14,14 @@ public class C05_ExceliMapeYukleme {
         String path="src/test/java/resources/ulkeler.xlsx";
         String sayfaAdı="Sayfa1";
 
-        System.out.println(ReusableMethods.mapOlustur(path, sayfaAdı));
+        Map<String,String> ulkelerMap=ReusableMethods.mapOlustur(path,sayfaAdı);
+
+        System.out.println(ulkelerMap.get("Turkey"));
+
+        Assert.assertTrue(ulkelerMap.containsKey("Netherlands"));
+
+
+
 
     }
 }
--- src/test/java/tests/day21/C01_WindowHandleReusableMethods.java
+++ src/test/java/tests/day21/C01_WindowHandleReusableMethods.java
@@ -1,8 +1,11 @@
 package tests.day21;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import utilities.Driver;
+
+import java.util.Set;
 
 public class C01_WindowHandleReusableMethods {
     @Test
@@ -11,7 +14,23 @@ public class C01_WindowHandleReusableMethods {
         String ilkSayfaWindowHandle=Driver.getDriver().getWindowHandle();
         Driver.getDriver().findElement(By.xpath("//button[@id='tabButton']")).click();
 
+        Set<String> handleSeti=Driver.getDriver().getWindowHandles();
 
+        String ıkıncıSayfaHandle="";
+
+        for (String each:handleSeti
+             ) {
+            if(!each.equals(ilkSayfaWindowHandle)){
+                ıkıncıSayfaHandle=each;
+            }
+        }
+
+        Driver.getDriver().switchTo().window(ıkıncıSayfaHandle);
+
+        String  actualTitle=Driver.driver.getTitle();
+        String expectedTitle="New Window";
+
+Assert.assertEquals(actualTitle,expectedTitle);
 
 
 
